276f6a85e8de126ae6caad5b32af8563aecb14f5
Ajay.Bansode <Ajay.Bansode@DESKTOP-B7N6PTK>
2023-07-04 09:57:58 +0530

    Add project files.

---
+++ WpfAppExpDateConverter/ExpDateConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfAppExpDateConverter
+{
+    public class ExpDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            DateTime dt = (DateTime)value;
+            if (dt < DateTime.Now)
+            {
+                parameter = Brushes.Red;
+            }
+            else
+            {
+                parameter = Brushes.Green;
+            }
+            return parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
